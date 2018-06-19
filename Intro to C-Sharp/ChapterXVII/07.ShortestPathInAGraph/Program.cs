998ab4cc4e4fbba7f2b2be4469eb00b0bd2bdd36
George Georgiev <g.simeonov@apis.bg>
2018-06-19 08:26:15 +0300

    Deleted a bunch of things

--- Intro to C-Sharp/ChapterXVII/07.ShortestPathInAGraph/Program.cs
+++ Intro to C-Sharp/ChapterXVII/07.ShortestPathInAGraph/Program.cs
@@ -10,15 +10,67 @@
     {
         public static void Main(string[] args)
         {
-            string filePath = @"../../tinyG.txt";
-            Graph g = new Graph(filePath);
-            Console.WriteLine(g.ToString());
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int currentRow = 0;
+            int currentCol = 0;
+            int iterator = 0;
+            int cnt = 1;
 
-            LinkedList<int> adj = g.GetAdjacentVertices(0);
-
-            foreach (var vertex in adj)
+            while (cnt <= n * n)
             {
-                Console.WriteLine(vertex);
+                while (currentCol < (matrix.GetLength(1) - iterator))
+                {
+                    matrix[currentRow, currentCol] = cnt;
+                    cnt++;
+                    currentCol++;
+                }
+
+                currentCol--;
+                currentRow++;
+
+                while (currentRow < (matrix.GetLength(0) - iterator))
+                {
+                    matrix[currentRow, currentCol] = cnt;
+                    cnt++;
+                    currentRow++;
+                }
+                currentRow--;
+                currentCol--;
+
+                while (currentCol >= (0 + iterator))
+                {
+                    matrix[currentRow, currentCol] = cnt;
+                    cnt++;
+                    currentCol--;
+                }
+                currentCol++;
+                currentRow--;
+
+                while (currentRow >= (1 + iterator))
+                {
+                    matrix[currentRow, currentCol] = cnt;
+                    cnt++;
+                    currentRow--;
+                }
+
+                iterator++;
+                currentRow = iterator;
+                currentCol = iterator;
+            }
+
+            Print(matrix);
+        }
+
+        private static void Print(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i,j] + " ");
+                }
+                Console.WriteLine();
             }
         }
     }
