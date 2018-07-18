8a358ef5753a78d903548cc832c4501d7c8e9a85
George Georgiev <g.simeonov@apis.bg>
2018-07-18 16:50:28 +0300

    Solve problem 12 of Chapter XVII

--- Chapter XVII/12.SumOfFileSizeInSubtree/Program.cs
+++ Chapter XVII/12.SumOfFileSizeInSubtree/Program.cs
@@ -1,23 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _12.SumOfFileSizeInSubtree
 {
     public class File
     {
         public string Name { get; private set; }
-        public int Size { get; private set; }
+        public ulong Size { get; private set; }
+
+        public File(string name, long size)
+        {
+            this.Name = name;
+            this.Size = (ulong)size;
+        }
     }
 
     public class Folder
     {
         public string Name { get; private set; }
         public List<Folder> SubFolders { get; private set; }
-        public List<File> Files { private get; set; }
+        public List<File> Files { get; private set; }
 
         public Folder(string name)
         {
@@ -57,7 +60,7 @@ namespace _12.SumOfFileSizeInSubtree
             this.SubFolders.AddRange(folders);
         }
 
-        public void AddFolder(Folder folder)
+        public void AddSubFolder(Folder folder)
         {
             this.SubFolders.Add(folder);
         }
@@ -90,14 +93,60 @@ namespace _12.SumOfFileSizeInSubtree
             }
 
             Tree t = new Tree(new Folder(rootFolderPath));
-            DirectoryInfo rootFolder = new DirectoryInfo(rootFolderPath);
-            
+            DirectoryInfo rootDirInfo = new DirectoryInfo(rootFolderPath);
 
+            ConstructTree(rootDirInfo, t.Root);
+            ulong totalFileSize = 0UL;
+            GetTotalFileSizeInSubtree(t.Root, ref totalFileSize);
+            Console.WriteLine("Total size of the directory: " + totalFileSize + " bytes");
         }
 
-        public static void TraverseDepthFirst()
+        public static void ConstructTree(DirectoryInfo dirInfo, Folder folder)
         {
+            var directories = dirInfo.GetDirectories();
+            var files = dirInfo.GetFiles();
 
+            foreach (var file in files)
+            {
+                File f = new File(file.FullName, file.Length);
+                folder.AddFile(f);
+            }
+
+            foreach (var directory in directories)
+            {
+                Folder f = new Folder(directory.FullName);
+                folder.AddSubFolder(f);
+
+                ConstructTree(new DirectoryInfo(directory.FullName), f);
+            }
+        }
+
+        public static void TraverseDepthFirst(Folder root)
+        {
+            Console.WriteLine(root.Name);
+
+            foreach (var file in root.Files)
+            {
+                Console.WriteLine(file.Name);
+            }
+
+            foreach (var folder in root.SubFolders)
+            {
+                TraverseDepthFirst(folder);
+            }
+        }
+
+        public static void GetTotalFileSizeInSubtree(Folder root, ref ulong totalSize)
+        {
+            foreach (var file in root.Files)
+            {
+                totalSize += file.Size;
+            }
+
+            foreach (var folder in root.SubFolders)
+            {
+                GetTotalFileSizeInSubtree(folder, ref totalSize);
+            }
         }
     }
 }
